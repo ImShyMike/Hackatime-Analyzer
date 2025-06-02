d9fa9728df9be08147a1372fa59b33608d743b48
ShyMike <122023566+ImShyMike@users.noreply.github.com>
2025-06-02 21:50:30 +0100

    added catpuccin color palette
    why was this so hard, this took me like 2 hours of debugging package versions (turns out tailwind catpuccin did not support tailwind v4 D:)

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<h1 class="grid h-screen place-items-center text-5xl">
+	{page.status}: {page.error?.message}
+</h1>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main class="bg-ctp-base flex min-h-screen flex-col items-center justify-center">
+	<h1 class="text-ctp-mauve mb-4 text-5xl font-bold">Testing Catppuccin + Tailwind!</h1>
+	<p class="text-ctp-text mt-2">Please work :D</p>
+</main>
