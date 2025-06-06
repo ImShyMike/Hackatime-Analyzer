07ad4796ab7f260ece6dda01ed169e940e06f11d
ShyMike <122023566+ImShyMike@users.noreply.github.com>
2025-06-06 01:23:30 +0100

    Add graphs and some error handling

--- src/lib/interfaces.d.ts
+++ src/lib/interfaces.d.ts
@@ -1,12 +1,12 @@
 declare global {
     interface UserStats {
-            username?: string;
-            user_id?: string;
-            total_seconds?: number;
-            human_readable_total?: string;
-            daily_average?: number;
-            human_readable_daily_average?: string;
-        }
+        username?: string;
+        user_id?: string;
+        total_seconds?: number;
+        human_readable_total?: string;
+        daily_average?: number;
+        human_readable_daily_average?: string;
+    }
 
     interface Span {
         start_time: Date;
@@ -16,6 +16,11 @@ declare global {
 
     interface Day {
         date: Date;
+        sessions: Span[];
+    }
+
+    interface Hour {
+        hour: Date;
         sessions: Span[];
     }
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
-	import Dashboard from "../lib/components/dashboard.svelte";
+	import Dashboard from '../lib/components/Dashboard.svelte';
+	import { Toast } from 'flowbite-svelte';
 
-	let userId = $state("");
+	let userId = $state('');
+	let errorMessage = $state('');
 	let fetching = $state(false);
 	let fetchedStats = $state<UserStats>({});
 	let fetchedSpans = $state<Span[]>([]);
@@ -10,7 +12,11 @@
 		const response = await fetch(`https://corsproxy.io/?${url}`);
 
 		if (!response.ok) {
-			console.error("Failed to fetch data:", response.statusText);
+			console.error('Failed to fetch data:', response.statusText);
+			errorMessage = `User does not exist!`;
+			setTimeout(() => {
+				errorMessage = '';
+			}, 3000);
 			return;
 		}
 
@@ -19,14 +25,16 @@
 	}
 
 	async function loadStatsData() {
-		const data = await fetchUrl(`https://hackatime.hackclub.com/api/v1/users/${userId}/stats?features=a`);
+		const data = await fetchUrl(
+			`https://hackatime.hackclub.com/api/v1/users/${userId}/stats?features=a`
+		);
 
-		console.log("Fetched stats data:", data);
-	
+		console.log('Fetched stats data:', data);
+
 		if (!data) return;
 
 		if (!data.data || data.data.length === 0) {
-			console.error("No stats found for the user.");
+			console.error('No stats found for the user.');
 			return;
 		}
 
@@ -34,14 +42,16 @@
 	}
 
 	async function loadSpansData() {
-		const data = await fetchUrl(`https://hackatime.hackclub.com/api/v1/users/${userId}/heartbeats/spans`);
+		const data = await fetchUrl(
+			`https://hackatime.hackclub.com/api/v1/users/${userId}/heartbeats/spans`
+		);
 
-		console.log("Fetched spans data:", data);
+		console.log('Fetched spans data:', data);
 
 		if (!data) return;
 
 		if (!data.spans || data.spans.length === 0) {
-			console.error("No spans found for the user.");
+			console.error('No spans found for the user.');
 			return;
 		}
 
@@ -54,7 +64,7 @@
 
 	async function loadUserData() {
 		if (!userId) {
-			console.error("User ID is required.");
+			console.error('User ID is required.');
 			return;
 		}
 
@@ -65,21 +75,32 @@
 	}
 </script>
 
-<main class="bg-ctp-base pt-10 flex flex-col items-center justify-center">
+<main class="bg-ctp-base flex flex-col items-center justify-center pt-10">
 	<h1 class="text-ctp-mauve mb-4 text-5xl font-bold">Hackatime Analyzer</h1>
 	<p class="text-ctp-text mt-2">Easily analyze hackatime data in your browser!</p>
 	<input
 		type="text"
+		placeholder="Hackatime/Slack user ID"
 		bind:value={userId}
-		class="mt-4 rounded border border-ctp-mauve bg-ctp-base p-2 text-ctp-text"
+		class="border-ctp-mauve bg-ctp-base text-ctp-text mt-4 rounded border p-2"
 	/>
 	<button
 		disabled={fetching}
 		onclick={() => loadUserData()}
-		class="mt-4 rounded bg-ctp-mauve px-4 py-2 text-ctp-base hover:bg-ctp-mauve/80"
-	>Analyze</button>
+		class="bg-ctp-mauve text-ctp-base hover:bg-ctp-mauve/80 mt-4 cursor-pointer rounded px-4 py-2"
+		>Analyze</button
+	>
 	{#if Object.keys(fetchedStats).length > 0}
 		<Dashboard stats={fetchedStats} spans={fetchedSpans} />
 	{/if}
+	{#if errorMessage}
+		<div class="fixed top-5 right-5 z-50">
+			<Toast dismissable={false} class="bg-ctp-crust border-ctp-red rounded-lg border-2 shadow-lg">
+				<div class="mr-3 text-sm font-normal">
+					<span class="text-ctp-red mb-1 block font-semibold">Error</span>
+					<p class="text-ctp-text">{errorMessage}</p>
+				</div>
+			</Toast>
+		</div>
+	{/if}
 </main>
-	
